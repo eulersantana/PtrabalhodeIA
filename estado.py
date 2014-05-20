69f3a5a7220781f5219b52f625ac38f19b659575
Euler Santana <euler.santana@yahoo.com>
2014-05-19 23:41:37 -0300

    up criacao de class

--- estado.py
+++ estado.py
@@ -1,13 +1,21 @@
 class Posicao:
+	
 	def __init__(self,x,y):
 		self.x = x
 		self.y = y
 
 
 class Componente:
-	def __init__(self, cor, posicao,visitado = False, custo = 1, avaliacao):
+
+	def __init__(self, cor, posicao):
 		self.cor 		= cor
 		self.posicao 	= posicao
-		self.visitado 	= visitado
-		self.custo 		= custo
-		self.avaliacao 	= avaliacao
+		
+
+class Estado:
+
+	def __init__(self, componentes,visitado = False, custo = 1, avaliacao):
+		self.componentes 	= componentes		
+		self.visitado 		= visitado
+		self.custo 			= custo
+		self.avaliacao 		= avaliacao
