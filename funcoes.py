074535ba1690eeb17f6cf37fd39ac46cb7f83ba2
Euler Santana <euler.santana@yahoo.com>
2014-05-20 01:36:15 -0300

    inicio da implementacao de geraEstado

--- funcoes.py
+++ funcoes.py
@@ -1,8 +1,11 @@
 # gerador de estado
 
 def geraEstados(estadoAtual,movimento,tabuleiro):
-	if movimento == cima:
-		
+	novoComponentes = []
+	for pos in tabuleiro:		
+		if estadoAtual.componentes[0].posicao.x -1 == pos.posicao.x  and if estadoAtual.componentes[0].posicao.y -1 == pos.posicao.y 
+			if len(novo)
+
 		
 
 
